5fbccddabbfced82cff03ba501eb92dd5ebc3e2f
Davi Thomeny Cotting <122418493+davicotting@users.noreply.github.com>
2026-05-01 19:05:59 -0300

    feat: enable cors for frontend integration

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bodyParser: false,
   });
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
   const configService = app.get(ConfigService<Env, true>);
   const port = configService.get('PORT', { infer: true });
   await app.listen(port);
